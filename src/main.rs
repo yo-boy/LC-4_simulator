2c7bdcc13cde77a6e766be6ced26efff221fa594
yo_boi <abdelrahmanbbb@gmail.com>
2024-03-17 20:12:10 +0300

    removed uneeded test code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+#![allow(unused_imports)]
 mod prng;
 mod reader;
 use prng::{ASG, LFSR};
@@ -31,37 +32,6 @@ struct Machine {
 }
 
 fn main() {
-    println!("Hello, world!");
-    let state: u16 = 0xACE1;
-    let mut just_lfsr: LFSR = LFSR::new();
-    let mut lfsr_orig: LFSR = LFSR::new();
-    just_lfsr.set_seed(state);
-    lfsr_orig.set_seed(state);
-    just_lfsr.clock();
-    // 128 bits because the ASG overflows lower values
-    let mut period: u128 = 1;
-    while just_lfsr != lfsr_orig {
-        just_lfsr.clock();
-        period += 1;
-    }
-    println!("LSFR period: {}", period);
-
-    let first: u16 = 0xDA0C;
-    let second: u16 = 0xFD0B;
-    let mut asg: ASG = ASG::new();
-    let mut asg_orig: ASG = ASG::new();
-    asg.set_seed(state, first, second);
-    asg_orig.set_seed(state, first, second);
-    period = 1;
-    // asg.clock();
-    // while asg != asg_orig {
-    //     asg.clock();
-    //     period += 1;
-    // }
-    // println!("ASG period: {}", period);
-    let test: [u16; 65536] = [0u16; 2usize.pow(16)];
-    println!("{}", test[0]);
-
     let file = PathBuf::from("./examples/out.bin");
 
     let mut files: Vec<PathBuf> = Vec::new();
