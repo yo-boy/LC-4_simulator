d5f18525aed82ecb64fd7e2e60abae818af275d5
yo_boi <abdelrahmanbbb@gmail.com>
2024-03-18 12:55:43 +0300

    continued work on the simulation

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,33 @@ struct Machine {
     psr: PSR,
 }
 
+impl Machine {
+    pub fn new() -> Machine {
+        Machine {
+            asg: ASG::new(),
+            memory: [0b0u16; 65536],
+            pc: 0,
+            r0: 0,
+            r1: 0,
+            r2: 0,
+            r3: 0,
+            r4: 0,
+            r6: 0,
+            r7: 0,
+            usp: 0xFDFF,
+            ssp: 0x2FFF,
+            psr: PSR {
+                priority: u3::new(0),
+                supervisor: false,
+                n: false,
+                p: false,
+                z: false,
+            },
+        }
+    }
+    fn simulate_instruction(&mut self) {}
+}
+
 fn main() {
     let file = PathBuf::from("./examples/out.bin");
 
@@ -38,7 +65,64 @@ fn main() {
     files.push(file);
     let out = read_input_files(files);
 
-    for i in 0x3000..0x3010 {
+    for i in 0x3000..0x3011 {
         println!("0x{:04x}: {:016b}", i, out[i]);
+    }
+}
+
+mod tokenizer {
+    pub enum Operation {
+        ADD,
+        ADDi,
+        ADDi16,
+        ADDa,
+        AND,
+        ANDi,
+        ANDi16,
+        ANDa,
+        XOR,
+        XORi,
+        XORi16,
+        XORa,
+        BR,
+        JUMP,
+        RET,
+        JSR,
+        JSRR,
+        LD,
+        LDa,
+        ST,
+        STR,
+        STR16,
+        NOT,
+        TRAP,
+        RTI,
+        LSD,
+        LPN,
+        CLRP,
+        HALT,
+        PUTS,
+        GETC,
+        OUT,
+        IN,
+        PUTSP,
+    }
+
+    pub(crate) fn match_opcode_short(instruction: u16) -> Operation {
+        match instruction >> 11 & 0b11111 {
+            _ => todo!(),
+        }
+    }
+
+    pub(crate) fn match_opcode_long(instruction: u16) -> Operation {
+        match instruction >> 11 & 0b11111 {
+            _ => todo!(),
+        }
+    }
+
+    pub(crate) fn tokenize(encoded_instruction: u16, second_operand: Option<u16>) {
+        if encoded_instruction >> 10 & 0b1 == 0 {
+        } else {
+        }
     }
 }
